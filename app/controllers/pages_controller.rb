f4adfafece29b964eb30c25b6607693ef421a5b7
K C <aries0420@gmail.com>
2013-08-29 22:51:01 -0700

    Added favorite movie and year on home page for homework 4.2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
 
   def home
+    @my_favorite = Movie.find_by(title: "Titanic").title
+    @my_fav_year = Movie.find_by(title: "Titanic").year
   end
 
 end
